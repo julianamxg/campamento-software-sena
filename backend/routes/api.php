ea951f55f79bd6d76517e6897cde1015df07ceb7
julianamxgm <jamelo827@misena.edu.co>
2022-08-17 17:36:56 -0500

    primeras rutas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BootcampController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('prueba', function(){
+    echo "hola";
 });
+
+//Ruta rest para la gestion de estados
+//de los bootcamps
+Route::apiResource('bootcamps', BootcampController::class);
